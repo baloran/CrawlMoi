1fbe34eeeff62739e2d2d882aa338f48687a2a84
baloran <baloranandco@gmail.com>
2012-04-23 14:03:59 +0200

    creation du fichier search.php pour lister les sites réferencer

--- search.php
+++ search.php
@@ -1,1 +1,8 @@
-<?php
+<?php require_once 'core/db.php';?>
+<?php 
+$req_liste = $db->query('SELECT * FROM url');
+while($search = $req_liste->fetch()){
+echo $search['url'].'<br />'; 
+}
+
+$req_liste->closeCursor();
