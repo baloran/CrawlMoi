796cae990b8aed39d87d63e2fa9e8130d2d6d878
baloran <baloranandco@gmail.com>
2012-04-25 21:05:40 +0200

    rajout des fichiers index.php a la racine de chaque dossier pour éviter de pouvoir lister ce qu'il y a dedans. Rajout du dossier webroot avec les dossier img, css, js + du loader.php pour pouvoir charger ce qu'il y a dedans

--- load.php
+++ load.php
@@ -1,4 +1,18 @@
 <?php
+/*
+ * Fichier load.php inclue les fichier généraux qui peuvent être inclus de cette facon
+ * Il contient les PATH aussi
+ */
+
+// Path de la racine du site
+define("SITEPATH", 'http://localhost/lab/crawler/');
+
+// Path du dossier core
+define("CORE", SITEPATH."core/");
+
+// Path pour le dossier webroot
+define("WEBROOT", SITEPATH."webroot/");
+
 require_once 'simple_html_dom.php';
 require_once 'core/db.php';
 ?>
---
+++ webroot/loader.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * loader.php contient les variables d'environement pour les fichier javascript, css
+ * et d'images pour un meilleur développment
+ */
+
+// Path pour les fichiers CSS
+define("CSS", WEBROOT."css/");
+
+// Path pour les fichiers images
+define("IMG", WEBROOT."img/");
+
+// Path pour les fichiers JS
+define("JS", WEBROOT."js/");
